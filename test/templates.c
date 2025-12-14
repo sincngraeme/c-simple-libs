12417f582a206af3e24ab46ef13f42257e4164e3
Nigel Sinclair <sincngraeme@gmail.com>
2025-12-14 16:08:29 -0500

    don't remember

--- test/templates.c
+++ test/templates.c
@@ -10,9 +10,9 @@ U function_##T##_to_##U(T my_variable) {     \
 MULTIGEN((int , float), (int, double));
 #undef TEMPLATE
 
-#define function(T, U) INFER(function, T, int, double)
+#define function(T) INFER(function, T, int, double)
 #define TEMPLATE(T)                                 \
-T function_##T(T my_variable, char unused) {        \
+T function_##T(T my_variable) {        \
    my_variable += 1;                                \
    return my_variable;                              \
 }
@@ -20,8 +20,12 @@ GENERATE(int, double);
 #undef TEMPLATE
 
 int main() {
+    char c = 'c';
     printf("Integer: %f\n", function_int_to_float(10));
-    printf("Float: %f\n", function_int_to_double(10));
+    printf("Double: %f\n", function_int_to_double(10));
+    // printf("Integer: %d\n", function_int(10, c));
+    // printf("Double: %f\n", function_double(10, c));
+    printf("Int: %f\n", function(10.0));
     return 0;
 }
 
