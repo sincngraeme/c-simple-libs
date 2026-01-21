76bd0cb4a961f54dd92cd2de5af23075045ec2e3
Nigel Sinclair <sincngraeme@gmail.com>
2026-01-20 21:39:27 -0500

    added access ptrs, renamed shared to sole

--- csl-smrtptrs.h
+++ csl-smrtptrs.h
@@ -3,26 +3,80 @@
 
 #ifdef SMRTPTR_IMPLEMENTATION
 
-unsigned int shared_ptr_nrefs = 0;
-
-void create_shared_ptr() {
-    shared_ptr_nrefs++;
-}
-void free_shared_ptr(void* smrtptr) {
-    void** _smrtptr = (void**)smrtptr;
-    shared_ptr_nrefs--;
-    if(shared_ptr_nrefs == 0) free(*_smrtptr);
-}
+/* @brief:  This is called implicitly at the end of the scope and does not require
+ * any reference management. do not assign to a raw ptr except as a function argument */
 void free_unique_ptr(void* smrtptr) {
     void** _smrtptr = (void **)smrtptr;
     free(*_smrtptr);
 }
+/* @brief: Creates a unique_ptr */
+#define unique_ptr(T) __attribute__((cleanup(free_unique_ptr))) T*
 
-#define shared_ptr(T)                           \
-    create_shared_ptr();                          \
-    __attribute__((cleanup(free_shared_ptr))) T* 
+#ifdef INIT_SOLE_PTR
 
-#define unique_ptr(T) __attribute__((cleanup(free_unique_ptr))) T* 
+unsigned int sole_ptr_nrefs = 0;
+
+/* @brief:  Sole ptr can only be created once, and must correspond to one
+ * address at a time. It is managed in global state and each reference does
+ * not itself have ownership. DO NOT USE RAW POINTER REFERENCES.
+ *      - Each new allocation requires the sole_ptr to go fully out of scope and
+ *        be freed first
+ */
+void create_sole_ptr() {
+    sole_ptr_nrefs++;
+}
+
+/* @brief:  Frees a sole ptr once the global reference count has become 0 */
+void free_sole_ptr(void* smrtptr) {
+    void** _smrtptr = (void**)smrtptr;
+    sole_ptr_nrefs--;
+    if(sole_ptr_nrefs == 0) free(*_smrtptr);
+}
+
+/* @breif:  Defines a sole_ptr and increments the global counter */
+#define sole_ptr(T)                           \
+    create_sole_ptr();                          \
+    __attribute__((cleanup(free_sole_ptr))) T*
 
 #endif
 
+/* @brief:  Frees an access ptr once the reference count becomes 0 */
+void free_access_ptr(void* smrtptr) {
+    void** _smrtptr = (void**)smrtptr;
+    sole_ptr_nrefs--;
+    if(sole_ptr_nrefs == 0) free(*_smrtptr);
+}
+
+/* @brief:  Defines a sole_ptr and increments the global counter */
+#define access_ptr(T, group_name)                               \
+    create_##group_name##_access_ptr();                         \
+    __attribute__((cleanup(free_##group_name##_access_ptr))) T*
+
+#ifdef ACCESS_PTR_REGISTRY
+
+/* Generate struct definition */
+#define REGISTER_ACCESS_PTR(T, group_name) unsigned int group_name;
+
+struct Access_ptr_registry {
+    ACCESS_PTR_REGISTRY
+} access_ptr_registry = {0};
+
+#undef REGISTER_ACCESS_PTR
+
+/* Generate function definitions */
+#define REGISTER_ACCESS_PTR(T, group_name)                          \
+    void create_##group_name##_access_ptr() {                       \
+        access_ptr_registry.group_name++;                           \
+    }                                                               \
+    void free_##group_name##_access_ptr(T** smrtptr) {              \
+        access_ptr_registry.group_name--;                           \
+        if(access_ptr_registry.group_name == 0) free(*smrtptr);     \
+    }
+
+ACCESS_PTR_REGISTRY
+
+#undef REGISTER_ACCESS_PTR
+#endif
+
+
+#endif
--- test/smrtptrs.c
+++ test/smrtptrs.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #define SMRTPTR_IMPLEMENTATION
+
+#define INIT_SOLE_PTR
+#define ACCESS_PTR_REGISTRY \
+    REGISTER_ACCESS_PTR(int, group1)
+
 #include "../csl-smrtptrs.h"
 
 
@@ -10,15 +15,26 @@ int main() {
         printf("ptr1: %d\n", *ptr1);
     }
     {
-        shared_ptr(int) ptr2 = malloc(3 * sizeof(int));
+        sole_ptr(int) ptr2 = malloc(sizeof(int));
         *ptr2 = 2;
         printf("ptr2: %d\n", *ptr2);
         {
-            shared_ptr(int) ptr3 = ptr2;
+            sole_ptr(int) ptr3 = ptr2;
             printf("ptr2: %d\n", *ptr2);
             printf("ptr3: %d\n", *ptr3);
         }
         printf("ptr2: %d\n", *ptr2);
+    }
+    {
+        access_ptr(int, group1) ptr4 = malloc(sizeof(int));
+        *ptr4 = 4;
+        printf("ptr4: %d\n", *ptr4);
+        {
+            access_ptr(int, group1) ptr5 = ptr4;
+            printf("ptr4: %d\n", *ptr4);
+            printf("ptr5: %d\n", *ptr5);
+        }
+        printf("ptr4: %d\n", *ptr4);
     }
     return 0;
 }
