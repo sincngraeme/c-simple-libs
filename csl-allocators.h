2abcd113b22f7537c7bfa23b158fcc13b35e9269
Nigel Sinclair <sincngraeme@gmail.com>
2026-02-18 22:38:36 -0500

    object allocator, release function does not work

--- csl-allocators.h
+++ csl-allocators.h
@@ -1,93 +1,116 @@
 #include <stdlib.h>
 #include "csl-errval.h"
 
-#ifdef OBJECT_ALLOCATOR_TYPES
+#ifdef CSL_ALLOCATORS_IMPLEMENTATION
 
-typedef enum {
-#define OBJECT_ALLOCATOR_TYPE(T) ARENA_TYPE_##T,
-        OBJECT_ALLOCATOR_TYPES
-#undef OBJECT_ALLOCATOR_TYPE
-} Object_arena_types;
-
-typedef union {
-#define OBJECT_ALLOCATOR_TYPE(T) const T* T##_option;
-    OBJECT_ALLOCATOR_TYPES
-#undef OBJECT_ALLOCATOR_TYPE
-        void* _generic;  // Internal
-} Object_arena_options;
-
-/* Generic Arena Type */
 typedef struct {
     size_t nobjects;
     size_t object_size;
     size_t* index_stack;
-    Object_arena_types type;
-    /* Contains all types */
-    Object_arena_options u;
-} Object_arena;
+    size_t index_stack_size;
+    void* objects;
+} Arena;
 
-const size_t object_arena_index_stack_max = 100;
+typedef void* Arena_object; // Unfortunately necessary for the type mangling to work
 
-DERIVE_RESULT_DIRECT(Object_arena);
+/* For error wrapping */
+DERIVE_RESULT_DIRECT(Arena);
+DERIVE_RESULT_DIRECT(Arena_object);
 
-static RESULT(Object_arena) _make_object_arena(size_t nobjects, Object_arena_types type) {
-    size_t object_size = 0;
-    size_t index_stack_size = 0;
-    size_t arena_size = 0;
-    size_t allocation_size = 0;
+/* Initializes an arena for <nobjects> number of objects of fixed <object_size> size */
+static RESULT(Arena) init_arena(size_t nobjects, size_t object_size) {
+    size_t index_stack_size = sizeof(size_t) * ( nobjects < 100 ? nobjects : 100 ); 
+    size_t arena_size = nobjects * object_size; 
+    /* We allocate space for both the arena and the index stack */
+    size_t allocation_size = arena_size + index_stack_size; 
 
-    /* Switch on all derived types to ensure we get the right object size */
-    switch(type) {
-#define OBJECT_ALLOCATOR_TYPE(T)        \
-        case ARENA_TYPE_##T:            \
-            object_size = sizeof(T);    \
-            break;
-        OBJECT_ALLOCATOR_TYPES
-#undef OBJECT_ALLOCATOR_TYPE
-        default: return ERR(Object_arena, (Object_arena){0});
-    }
+    void* pobjects = IFNULL(malloc(allocation_size), {
+        return ERR( Arena, (Arena){0} );
+    });
 
-    /* The index stack size is capped at 100 to prevent 
-     * unneccessary large allocations (it will get realloced if
-     * necessary) */
-    index_stack_size = ( (nobjects < object_arena_index_stack_max) 
-                        ? nobjects 
-                        : object_arena_index_stack_max );
-    arena_size = object_size * nobjects;
-    allocation_size = arena_size + index_stack_size;
-
-    /* Allocate space for the objects and the index stack */
-    void* pobjects = NULL; 
-    if((pobjects = malloc(allocation_size)) == NULL) {
-        return ERR(Object_arena, (Object_arena){0});
-    }
-    Object_arena arena = {
+    Arena arena = {
         .nobjects = nobjects,
         .object_size = object_size,
-        /* Index stack will be located at the end of all the objects */
-        .index_stack = pobjects + allocation_size,
-        .type = type,
-        /* here we use the void pointer version */
-        .u._generic = pobjects
+        .objects = pobjects,
+        .index_stack = pobjects + arena_size,
+        .index_stack_size = index_stack_size,
     };
-    return OK(Object_arena, arena);
+    *arena.index_stack = 0; // Starting at the block of memory
+    return OK( Arena, arena );
 }
 
-static RESULT(Object_arena_options) _object_arena_alloc(Object_arena* arena) {
-    if(arena == NULL) {
-        
+/* Returns a pointer to an object in the allocated arena space */
+__attribute__((unused)) // TEMP: just for debugging
+static RESULT(Arena_object) arena_alloc(Arena* arena) {
+    if(
+        arena->nobjects         == 0    ||
+        arena->object_size      == 0    ||
+        arena->objects          == NULL ||
+        arena->index_stack      == NULL ||
+        arena->index_stack_size == 0    ||
+        // Index stack pointer must be after the end of the objects
+        (void*)arena->index_stack < arena->objects + (arena->nobjects * arena->object_size) ||
+        // Index stack value cannot be greater than the number of allocated blocks
+        *arena->index_stack > arena->nobjects
+    ) return ERR(Arena_object, NULL); 
+    void* pobject = NULL;
+    size_t* stack_bottom = arena->objects + (arena->nobjects * arena->object_size);
+    /* Check if the stack is empty (we are appending) */
+    if(*arena->index_stack < *stack_bottom) {
+        /* In this case we are backfilling into the space that was allocated, 
+         * then released. Therefore, we must get the value at the top of the 
+         * index stack and decrement the index stack pointer */
+        pobject = arena->objects + *(arena->index_stack--);
+        return OK(Arena_object, pobject);
+    } else {
+        /* value pointed to by index stack is the index of the next 
+         * block to be allocated, in this case the index stack is empty 
+         * so we can just use the top value and increment the index */
+        pobject = arena->objects + arena->object_size * (*arena->index_stack)++;
+        return OK(Arena_object, pobject);
     }
 }
 
+/* Hands out a block of memory that was allocated with arena_init */
+[[nodiscard]] __attribute__((unused)) static int arena_release(Arena_object object, Arena* arena) {
+    if(
+        object                  == NULL ||
+        arena->nobjects         == 0    ||
+        arena->object_size      == 0    ||
+        arena->objects          == NULL ||
+        arena->index_stack      == NULL ||
+        arena->index_stack_size == 0    ||
+        // Index stack pointer must be after the end of the objects
+        (void*)arena->index_stack < arena->objects + (arena->nobjects * arena->object_size) ||
+        // Index stack pointer must be before the end of the allocated space
+        (void*)arena->index_stack > ( arena->objects + (arena->nobjects * arena->object_size) + arena->index_stack_size * sizeof(size_t) ) ||
+        // Index stack value cannot be greater than the number of allocated blocks
+        *arena->index_stack > arena->nobjects ||
+        // Object must be in bounds of this arena's object space
+        object < arena->objects ||
+        object > arena->objects + (arena->nobjects * arena->object_size)
+    ) return 1; 
+    /* TODO: Handle index stack overflow */
+    size_t* stack_bottom = arena->objects + (arena->nobjects * arena->object_size);
+    /* Find the index of the given object */
+    size_t obj_index = (size_t)( ( object - arena->objects ) / arena->object_size );
+    if(obj_index < *stack_bottom) {
+        /* The object that is being released is not at the end so we must push
+         * its index onto the stack so the memory can be used again */
+        *(++arena->index_stack) = obj_index;
+    } else {
+        /* The index of the object is at the end of the objects, so we just decrement the 
+         * value at the bottom of the stack (corresponds to the end of the objects) */
+        (*stack_bottom)--;
+    }
+    return 0;
+}
 
-
-static void free_object_arena(Object_arena* arena) {
-    free(arena->u._generic);
+static void free_arena(Arena* arena) {
+    free(arena->objects);
     arena->nobjects = 0;
     arena->object_size = 0;
     arena->index_stack = NULL;
 }
-
-#define make_object_arena(T, nobjects) _make_object_arena(nobjects, ARENA_TYPE_##T)
 
 #endif
--- test/allocators.c
+++ test/allocators.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 typedef struct {
@@ -5,19 +6,65 @@ typedef struct {
     int y;
 } point;
 
-#define OBJECT_ALLOCATOR_TYPES \
-    OBJECT_ALLOCATOR_TYPE(point)
+#define CSL_ALLOCATORS_IMPLEMENTATION
 
 #include "../csl-allocators.h"
 
-#define defer(dealloc) __attribute__((cleanup(dealloc)))
+#define defer(fn) __attribute__((cleanup(fn)))
 
 int main() {
-    defer(free_object_arena) Object_arena arena = UNWRAP(make_object_arena(point, 10), {
+    defer(free_arena) Arena arena = UNWRAP(init_arena(10, sizeof(point)), {
         fprintf(stderr, "ERROR: Failed to allocate arena.\n");
         return -1; 
     });
-    printf("Hello from "__FILE__);
-    // free_object_arena(&arena);
+    point* p1 = UNWRAP( arena_alloc(&arena), {
+        fprintf(stderr, "ERROR: Failed to allocate object from arena memory.\n");
+        return -1;
+    });
+    p1->x = 10;
+    p1->y = 15;
+    printf("Point p1 (%p): x = %d, y = %d\n", p1,  p1->x, p1->y);
+
+    point* p2 = UNWRAP( arena_alloc(&arena), {
+        fprintf(stderr, "ERROR: Failed to allocate object from arena memory.\n");
+        return -1;
+    });
+    p2->x = 20;
+    p2->y = 25;
+    printf("Point p1 (%p): x = %d, y = %d\n", p1,  p1->x, p1->y);
+    printf("Point p2 (%p): x = %d, y = %d\n", p2,  p2->x, p2->y);
+    
+    if(arena_release(p1, &arena)) {
+        fprintf(stderr, "ERROR: Failed to release object in arena.\n");
+        return -1;
+    };
+    point* p3 = UNWRAP( arena_alloc(&arena), {
+        fprintf(stderr, "ERROR: Failed to allocate object from arena memory.\n");
+        return -1;
+    });
+    assert(p1 == p3);
+    p3->x = 30;
+    p3->y = 35;
+    printf("Point p1 (%p): x = %d, y = %d\n", p1,  p1->x, p1->y);
+    printf("Point p2 (%p): x = %d, y = %d\n", p2,  p2->x, p2->y);
+    printf("Point p3 (%p): x = %d, y = %d\n", p3, p3->x, p3->y);
+    
+    if(arena_release(p2, &arena)) {
+        fprintf(stderr, "ERROR: Failed to release object in arena.\n");
+        return -1;
+    };
+
+    point* p4 = UNWRAP( arena_alloc(&arena), {
+        fprintf(stderr, "ERROR: Failed to allocate object from arena memory.\n");
+        return -1;
+    });
+    assert(p4 == p2);
+    p4->x = 40;
+    p4->y = 45;
+    printf("Point p1 (%p): x = %d, y = %d\n", p1,  p1->x, p1->y);
+    printf("Point p2 (%p): x = %d, y = %d\n", p2,  p2->x, p2->y);
+    printf("Point p3 (%p): x = %d, y = %d\n", p3, p3->x, p3->y);
+    printf("Point p4 (%p): x = %d, y = %d\n", p4, p4->x, p4->y);
+
     return 0;
 }
